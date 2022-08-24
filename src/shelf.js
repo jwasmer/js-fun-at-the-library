0d54e8c756ee9f2c1f1f346fec906931837ab161
James Wasmer <jameswasmer@gmail.com>
2022-08-24 19:52:22 -0400

    Construct functions to pass shelf-test.js tests.

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,44 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+  shelf.unshift(book)
+  }
+}
+
+function unshelfBook(book, shelf) {
+  for (i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book) {
+      shelf.splice(i, 1)
+    }
+  }
+}
+
+function listTitles(shelfName) {
+  var titlesOnShelf = null
+  for (i = 0; i < shelfName.length; i++) {
+    if (titlesOnShelf === null) {
+      titlesOnShelf = `${shelfName[i].title}`
+    }
+    else {
+      titlesOnShelf = `${titlesOnShelf}, ${shelfName[i].title}`
+    }
+  }
+  console.log(titlesOnShelf)
+  return titlesOnShelf
+}
+
+function searchShelf(shelf, book) {
+  var isOnShelf = false
+  for (i = 0; i < shelf.length; i++) {
+    if (book === shelf[i].title) {
+      isOnShelf = true
+    }
+  }
+  return isOnShelf
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
