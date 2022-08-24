ecda5977123a2b345e58182d2d88227e18943c9b
James Wasmer <jameswasmer@gmail.com>
2022-08-24 18:25:57 -0400

    Construct functions to resolve book-test.js tests.

--- src/book.js
+++ src/book.js
@@ -1,12 +1,52 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(reviewText, reviewsHolder) {
+
+  var isUnique = true
+
+  for (i = 0; i < reviewsHolder.length; i++) {
+    if (reviewsHolder[i] === reviewText) {
+      isUnique = false
+    }
+  }
+  if (isUnique === true) {
+    reviewsHolder.push(reviewText)
+  }
+}
+
+function calculatePageCount(title) {
+  var bookLength = title.length * 20
+  return bookLength
+}
+
+function writeBook(title, character, genre) {
+  return {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
