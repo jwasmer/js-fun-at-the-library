27cb13fc9a4864ee3bb808f5970131049455e2e0
James Wasmer <jameswasmer@gmail.com>
2022-08-25 19:36:20 -0400

    Stage src/Library.js file

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,37 @@
+function createLibrary (name) {
+  return {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+}
 
+function addBook (library, book) {
+  var bookGenre = book.genre
+  library.shelves[bookGenre].push(book)
+}
+
+function checkoutBook (library, book, genre) {
+  var isBookShelved = false
+  for (i = 0; i < library.shelves[genre].length; i++) {
+    if (library.shelves[genre][i].title === book) {
+      library.shelves[genre].splice(i, 1)
+      isBookShelved = true
+    }
+  }
+  if (isBookShelved === false) {
+    return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
+  }
+  else {
+    return(`You have now checked out ${book} from the ${library.name}`)
+  }
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
